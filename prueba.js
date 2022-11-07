ae1b2e8fa4c830c25e2e62c650b4692e9f83e3de
Ayrton Miotti <103609836+AyrtonMiotti@users.noreply.github.com>
2022-11-07 20:44:17 -0300

    Updates
    Updates

--- prueba.js
+++ prueba.js
@@ -1,11 +1,29 @@
-let DNI =  "3030456"
+let DNI =  "45095310"
+asd = "*****"
+var newDNI = DNI.split("");
 
-let sec =  DNI;
-//let separator = 
 
-DNI.split(DNI, 5);
-let asd = DNI.replaceAll(sec, '*')
+for(let i=5; i<8; i++){
+    asd += newDNI[i]
+}
+
+console.log(asd);
+
+
+
+/*
+var cant = (sec.length - 3);
+
+asd = []
+console.log('----------------')
+for(let i = 0; i<cant; i++){
+    asd += newDNI[i].replace(sec[i], '*')
+    console.log(asd)
+}
+console.log('----------------')
+
 
 console.log(DNI)
 console.log(sec)
 console.log(asd);
+*/

ea7de8dde7768e31b015f523f7cdb2a42b2a3a13
Ayrton Miotti <103609836+AyrtonMiotti@users.noreply.github.com>
2022-11-07 23:10:12 -0300

    Update
    Update

--- public/database/db.js
+++ public/database/db.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-    host: "localhost",
+    host: "dpg-cdkraikgqg43pc4kgvg0-a",
     user: "root",
     password: "",
     database: "",
